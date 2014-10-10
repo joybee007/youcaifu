17bcdad5e8b0ca016dcae97ff2bd4ead26483450
joybee007 <yuhao007007007@163.com>
2014-10-10 14:32:58 +0800

    找回密码修改

--- js/findPass.js
+++ js/findPass.js
@@ -1,4 +1,19 @@
 // 找回密码页面js
 $(function(){
+	$('#mobile').click(function(){
+		$('#mobileFind').show();
+		$('#emailFind').hide();
+	});
+	$('#email').click(function(){
+		$('#mobileFind').hide();
+		$('#emailFind').show();
+	});
+
+
+
+
+
+
+
 
 });
