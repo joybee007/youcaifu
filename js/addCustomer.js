7a8f88886ea6e1f71c6f89bb76e1fa5ad3b61164
joybee007 <yuhao007007007@163.com>
2014-10-11 17:42:07 +0800

    1

--- js/addCustomer.js
+++ js/addCustomer.js
@@ -1,2 +1,15 @@
 
-
+$(function(){
+	$('.slideBtn').click(function(){
+		$('.customer_more_info').slideToggle();
+		$(this).toggleClass('slideBtn_slideDown');
+		toggleText(this);
+	});
+	function toggleText(node){
+		if(node.innerHTML=='收起'){
+			node.innerHTML='展开'
+		}else{
+			node.innerHTML='收起'
+		}
+	}
+});
