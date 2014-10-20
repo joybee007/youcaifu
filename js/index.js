d460e8ca0147ac3ee08880f98625601a5957098c
joybee007 <yuhao007007007@163.com>
2014-10-20 13:19:38 +0800

    新手引导

--- js/index.js
+++ js/index.js
@@ -7,6 +7,23 @@ $(function(){
 	$('.fadeImg li').hover(function(e){
 		$(this).find('img').stop(true,true).fadeToggle('fast');
 	});
-	
+	var xinshou_marquee=null;
+	//新手引导
+	function showXinshou(){
+		$('#mask,#xinshou').show();
+		if(!xinshou_marquee){
+			xinshou_marquee=$('#xinshou_marquee').kxbdSuperMarquee({
+				distance:440,
+				duration:40,
+				direction:'left',
+				isEqual:true,
+				btnGo:{left:'#xinshou .next_btn',right:'#xinshou .prev_btn'},
+				isMarquee:false,
+				navId:'#xinshou_marquee_nav'
+			});
+		}
+	}
+	//显示引导
+	showXinshou();
 	
 });
