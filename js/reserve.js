38331aadb3c0ea289f2dc4cc9334b16f8472063b
joybee007 <yuhao007007007@163.com>
2014-10-22 14:35:46 +0800

    预约页面交互修改

--- js/reserve.js
+++ js/reserve.js
@@ -1,1 +1,17 @@
+$(function(){
 
+	$('.old_list table tr').hover(function(){
+		$(this).toggleClass('bg-yellow').find('.edit_btn').toggle();
+	});
+	$('.old_list table :radio').click(function(){
+		if($(this).is('.add_new_radio')){
+			$(this).parents('.old_list').next('.add_new').show();
+		}else{
+			$(this).parents('.old_list').next('.add_new').hide();
+		}
+	});
+	$('.submit').click(function(){
+		$(this).addClass('submit_disabled').prev('.saved_info').show().prev('.add_new').hide().prev('.old_list').hide();
+	});
+	
+});
