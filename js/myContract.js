8e683d03e94a30e437255a11a6c7232e2593ad1e
joybee007 <yuhao007007007@163.com>
2014-10-23 14:41:19 +0800

    我的合同

---
+++ js/myContract.js
@@ -0,0 +1,15 @@
+
+$(function(){
+	$('.contract_status').hover(function(e){
+		$(this).find('.c_status_pop').toggle();
+	}).each(function(i,a){
+		$(a).css('z-index',50-i);
+	});
+	//$('.c_status_pop').click(function(){});
+	
+	
+	
+	
+	
+});
+
