8cf1e975d94ff27813189c917fc92f82d811a6c8
joybee007 <yuhao007007007@163.com>
2014-10-30 14:06:27 +0800

    datepicker

--- js/productCustomization.js
+++ js/productCustomization.js
@@ -1,14 +1,6 @@
 
 $(function(){
-	$('#time').datetimepicker({
-		lang : 'ch',
-		timepicker : true,
-		format : 'Y-m-d H:i:00',
-		formatDate : 'Y-m-d',
-		formatTime : 'H:i:00',
-		allowBlank:true,
-		step:1
-	});
+	$('#time').focus(WdatePicker);
 
 
 
