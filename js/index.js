674a2689482610409a49cfeb909dd8c8bdee864d
joybee007 <yuhao007007007@163.com>
2014-11-13 11:19:23 +0800

    弹窗

--- js/index.js
+++ js/index.js
@@ -42,4 +42,33 @@ $(function(){
 	});
 	
 	
+	
+	layer.alert('提示信息', 8);
+	$.layer({
+		shade: [0],
+		area: ['auto','auto'],
+		dialog: {
+			msg: '您确定要继续？',
+			btns: 2,                    
+			type: 4,
+			btn: ['确认','取消'],
+			yes: function(){
+				layer.msg('确认', 1, 1);
+			},
+			no: function(){
+				layer.msg('取消', 1, 13);
+			}
+		}
+	});
+	/* $.layer({
+        type: 1,
+        title: false, //不显示默认标题栏
+        shade: [0], //不显示遮罩
+        area: ['600px', '360px'],
+        page: {html: '<img src="http://static.oschina.net/uploads/space/2014/0516/012728_nAh8_1168184.jpg" alt="layer">'}
+    }); */
+	
+	
+	
+	
 });
