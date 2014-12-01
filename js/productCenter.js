0d83635239812256d484358126df353097e5874a
joybee007 <yuhao007007007@163.com>
2014-12-01 09:54:48 +0800

    产品中心轮播

--- js/productCenter.js
+++ js/productCenter.js
@@ -6,13 +6,9 @@ $(function(){
 		isEqual:true,
 		btnGo:{left:'#pm_prev',right:'#pm_next'}
 	});
-	function pageScroll(){
-		window.scrollBy(0,-50);
-		var scrolldelay=setTimeout(arguments.callee,10);
-		0==document.documentElement.scrollTop&&0==document.body.scrollTop&&clearTimeout(scrolldelay);
-	}
-	$('#go_top').click(pageScroll);
-	
+	$('#banner_marquee').y_marquee({
+		nav:'#marqueeNav'
+	});
 	$('.pb_intro:first').show();
 	$('.pb_info').mouseenter(function(){
 		$(this).siblings().find('.pb_intro').slideUp('fast');
